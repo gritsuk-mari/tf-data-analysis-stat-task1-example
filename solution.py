3b55c20daa9b7026ce53e480728ef9501973edd9
gritsuk-mari <104819287+gritsuk-mari@users.noreply.github.com>
2023-04-07 21:02:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 232852966 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    t = 10 
+    x = (x - 49 + np.exp(1)) / t 
     return x.mean() # Ваш ответ
